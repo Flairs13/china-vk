591813e4ea1a4f88975d4233c17399a095606440
Flairs13 <flairs13@gmail.com>
2021-03-30 14:46:03 +0400

    Updates

---
+++ precache-manifest.641c4ecb2f5198e315349b5b20d4cea7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6536654269fb578d91f5956288726a1",
+    "url": "/china-vk/index.html"
+  },
+  {
+    "revision": "42dbe1bb37c588f91ef5",
+    "url": "/china-vk/static/css/main.fe78fc2d.chunk.css"
+  },
+  {
+    "revision": "191de9221c3f09b44316",
+    "url": "/china-vk/static/js/2.91ca4909.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/china-vk/static/js/2.91ca4909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42dbe1bb37c588f91ef5",
+    "url": "/china-vk/static/js/main.946092f4.chunk.js"
+  },
+  {
+    "revision": "86be212d18f499560eb2",
+    "url": "/china-vk/static/js/runtime-main.ff2dbf61.js"
+  },
+  {
+    "revision": "f685b5ddd537fd1a1fafe49865aa7e10",
+    "url": "/china-vk/static/media/Preloader.f685b5dd.svg"
+  },
+  {
+    "revision": "d39285dbf0106d6234f1594cd26eed24",
+    "url": "/china-vk/static/media/camera.d39285db.svg"
+  },
+  {
+    "revision": "6724e28006e6914078b7503f0dc09f56",
+    "url": "/china-vk/static/media/close-cross.6724e280.svg"
+  },
+  {
+    "revision": "a5815099dbc8ea9ab8a98b6417d8c10a",
+    "url": "/china-vk/static/media/color-palette.a5815099.svg"
+  },
+  {
+    "revision": "b04d99fae9464c8fd62c10e0b30f89ef",
+    "url": "/china-vk/static/media/facesad.b04d99fa.png"
+  }
+]);
